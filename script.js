c5b4ed96b45f30463f84eb7ccc9c73ab3e1f8e41
NinjaStrikers <14817642+ninjastrikers@users.noreply.github.com>
2025-12-07 13:09:53 +0630

    initial

---
+++ script.js
@@ -0,0 +1,12 @@
+// Smooth Scrolling
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+// Simple Console Log to check if it's working
+console.log("Ninja Strikers Website Loaded");
